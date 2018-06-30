cbf45fb515fe79bd4c670ebb6b1d3c387a98ee5f
ylyxf <304189773@qq.com>
2018-06-30 10:23:47 +0800

    完善initsql，不必和版本挂钩，滚动地增加sql即可

--- src/main/java/org/siqisource/stone/upgrade/InitSqlSchemaReader.java
+++ src/main/java/org/siqisource/stone/upgrade/InitSqlSchemaReader.java
@@ -4,25 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Set;
 
+import org.siqisource.stone.utils.ResourceUtil;
+
 public class InitSqlSchemaReader {
 
-	public static String SQL_TEST_VERSION = "";
+	public static String TABLE_EXECUTED_SQL = "STRT_EXECUTED";
 
-	public static String SQL_VERSION_IN_DB = "";
-
-	public static final LinkedHashMap<String, String> VERSION_SQL_STORE = new LinkedHashMap<String, String>();
-
-	public static String getInitSchemaFile(Properties properties, String appVersion) throws Exception {
-
-		List<String> sqlFiles = new ArrayList<String>();
-
+	public static String getInitSchemaFile(Properties properties, String appId) throws Exception {
+		System.out.println("initSqlFolder:::::");
 		// 连接数据库
 		String url = properties.getProperty("spring.datasource.url");
 		String username = properties.getProperty("spring.datasource.username");
@@ -32,55 +27,36 @@ public class InitSqlSchemaReader {
 		Connection conn = DriverManager.getConnection(url, username, password);
 
 		// 尝试读取是否存在配置表
-		Statement tableStatement = conn.createStatement();
-		ResultSet tableRs = tableStatement.executeQuery(SQL_TEST_VERSION);
-		tableRs.next();
-		Integer tableCount = tableRs.getInt(1);
-		tableRs.close();
-		tableStatement.close();
+		Set<String> executedSqlSet = new HashSet<String>();
+		try {
+			Statement tableStatement = conn.createStatement();
+			String sql = "select SQL_NAME from STRT_EXECUTED where APPID ='" + appId + "' order by EXECUTE_TIME asc";
+			ResultSet sqlRs = tableStatement.executeQuery(sql);
+			while (sqlRs.next()) {
+				String executedSql = sqlRs.getString(1);
+				executedSqlSet.add(executedSql);
+			}
+			sqlRs.close();
+			tableStatement.close();
+		} catch (Exception e) {
+			Statement tableStatement = conn.createStatement();
+			String sql = "create table STRT_EXECUTED (APPID varchar(128),SQL_NAME varchar(128),EXECUTE_TIME date,primary key(APPID,SQL_NAME))";
+			tableStatement.execute(sql);
+			tableStatement.close();
+		}
 
-		if (tableCount == 0) {
-			Set<Entry<String, String>> entrySet = VERSION_SQL_STORE.entrySet();
-			for (Entry<String, String> entry : entrySet) {
-				sqlFiles.add(entry.getValue());
-			}
-		} else {
-			// 从配置表中读取当前版本
-			String currentVersion = "";
-			Statement versionStatement = conn.createStatement();
-			ResultSet versionRs = versionStatement.executeQuery(SQL_VERSION_IN_DB);
-			if (versionRs.next()) {
-				currentVersion = versionRs.getString("version");
-			} else {
-				throw new Exception("无法查询到URPLUS版本号");
-			}
-			versionRs.close();
-			versionStatement.close();
-			conn.close();
-
-			if (currentVersion == null || "".equals(currentVersion)) {
-				throw new Exception("无法查询到URPLUS");
-			}
-
-			if (!VERSION_SQL_STORE.containsKey(currentVersion)) {
-				throw new Exception("URPLUS版本号'" + currentVersion + "'未定义");
-			}
-
-			if (greater(currentVersion, appVersion)) {
-				throw new Exception("数据库中的URPLUS版本号" + currentVersion + "高于当前程序版本" + appVersion + "，请下载最新版小助手'");
-			}
-			if (!appVersion.equals(currentVersion)) {
-				boolean foundVersionInDefine = false;
-				Set<Entry<String, String>> entrySet = VERSION_SQL_STORE.entrySet();
-				for (Entry<String, String> entry : entrySet) {
-					if (currentVersion.equals(entry.getKey())) {
-						foundVersionInDefine = true;
-						continue;
-					}
-					if (foundVersionInDefine) {
-						sqlFiles.add(entry.getValue());
-					}
-				}
+		// 读取jar包中的classes资源
+		ResourceUtil resourceUtil = new ResourceUtil();
+		List<String> sqlFiles = resourceUtil.listResources("initsql");
+		for (String sqlFile : sqlFiles) {
+			System.out.println("sqlFile:::::" + sqlFile);
+		}
+		// 剔除掉已执行的sql
+		Iterator<String> sqlFileIterator = sqlFiles.iterator();
+		while (sqlFileIterator.hasNext()) {
+			String sqlFile = sqlFileIterator.next();
+			if (executedSqlSet.contains(sqlFile)) {
+				sqlFileIterator.remove();
 			}
 		}
 
@@ -92,54 +68,6 @@ public class InitSqlSchemaReader {
 			initSchema = initSchema.substring(0, initSchema.length() - 1);
 		}
 		return initSchema;
-	}
-
-	public static boolean greater(String version1, String version2) throws Exception {
-		String[] version1Array = version1.split("[.]");
-		String[] version2Array = version2.split("[.]");
-
-		if (version1Array.length < 3) {
-			throw new Exception("版本号异常:" + version1);
-		}
-
-		if (version2Array.length < 3) {
-			throw new Exception("版本号异常" + version2);
-		}
-
-		int version1Main = 0;
-		int version1Middle = 0;
-		int version1Third = 0;
-		try {
-			version1Main = Integer.parseInt(version1Array[0]);
-			version1Middle = Integer.parseInt(version1Array[1]);
-			version1Third = Integer.parseInt(version1Array[2]);
-		} catch (NumberFormatException e) {
-			throw new Exception("版本号异常:x.y.z.other中，x/y/z应该为数字" + version1);
-		}
-
-		int version2Main = 0;
-		int version2Middle = 0;
-		int version2Third = 0;
-		try {
-			version2Main = Integer.parseInt(version2Array[0]);
-			version2Middle = Integer.parseInt(version2Array[1]);
-			version2Third = Integer.parseInt(version2Array[2]);
-		} catch (NumberFormatException e) {
-			throw new Exception("版本号异常:x.y.z.other中，x/y/z应该为数字" + version2);
-		}
-
-		if (version1Main > version2Main) {
-			return true;
-		} else if (version1Main == version2Main) {
-			if (version1Middle > version2Middle) {
-				return true;
-			} else if (version1Middle == version2Middle) {
-				if (version1Third > version2Third) {
-					return true;
-				}
-			}
-		}
-		return false;
 	}
 
 }
---
+++ src/main/java/org/siqisource/stone/utils/ResourceUtil.java
@@ -0,0 +1,107 @@
+package org.siqisource.stone.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.boot.ApplicationHome;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+public class ResourceUtil {
+
+	private String homePath = null;
+
+	private Boolean runInJar = true;
+
+	public String getHomePath() {
+		return homePath;
+	}
+
+	public Boolean getRunInJar() {
+		return runInJar;
+	}
+
+	public ResourceUtil() {
+		ApplicationHome home = new ApplicationHome(getClass());
+		File homeFolder = home.getSource();
+		String path = homeFolder.getAbsolutePath();
+		runInJar = !path.endsWith("classes");
+
+		if (runInJar) {
+			homePath = path.substring(0, path.lastIndexOf(System.getProperties().getProperty("file.separator")));
+		} else {
+			homePath = path.substring(0, path.length() - 8);
+		}
+	}
+
+	/**
+	 * 读取资源
+	 * 
+	 * @throws IOException
+	 */
+	public List<String> listResources(String folder) {
+		try {
+			if (!runInJar) {
+				ClassPathResource classPathResource = new ClassPathResource(folder);
+				File classFolder = classPathResource.getFile();
+				return Arrays.asList(classFolder.list());
+			} else {
+				List<String> resourceList = new ArrayList<String>();
+				ClassPathResource classPathResource = new ClassPathResource(folder);
+				String directoryToScan = classPathResource.getURL().toExternalForm();
+				ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+				Resource[] resources = resolver.getResources(directoryToScan + "/**");
+				for (Resource resource : resources) {
+					if (resource.exists() & resource.isReadable() && resource.contentLength() > 0) {
+						URL url = resource.getURL();
+						String urlString = url.toExternalForm();
+						String targetName = "classpath:/" + folder + urlString.substring(directoryToScan.length());
+						resourceList.add(targetName);
+					}
+				}
+				return resourceList;
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("读取资源列表的时候出错了", e);
+		}
+	}
+
+	/**
+	 * 释放资源
+	 * 
+	 * @throws IOException
+	 */
+	public void extraResources(String folder) {
+		try {
+			File destDir = new File(homePath);
+			if (!runInJar) {
+				ClassPathResource classPathResource = new ClassPathResource(folder);
+				FileUtils.copyDirectory(classPathResource.getFile(), new File(homePath));
+			} else {
+				ClassPathResource classPathResource = new ClassPathResource(folder);
+				String directoryToScan = classPathResource.getURL().toExternalForm();
+				ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+				Resource[] resources = resolver.getResources(directoryToScan + "/**");
+				for (Resource resource : resources) {
+					if (resource.exists() & resource.isReadable() && resource.contentLength() > 0) {
+						URL url = resource.getURL();
+						String urlString = url.toExternalForm();
+						String targetName = urlString.substring(directoryToScan.length());
+						File destination = new File(destDir, targetName);
+						FileUtils.copyURLToFile(url, destination);
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("释放资源的时候出错了", e);
+		}
+	}
+
+}
